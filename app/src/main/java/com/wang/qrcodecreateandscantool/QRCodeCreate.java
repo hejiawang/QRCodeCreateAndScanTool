b75b9b688dfffed2159edfc4f39715fc315fd1ff
hejiawang <952327407@qq.com>
2015-11-11 14:47:49 +0800

    test

--- app/src/main/java/com/wang/qrcodecreateandscantool/QRCodeCreate.java
+++ app/src/main/java/com/wang/qrcodecreateandscantool/QRCodeCreate.java
@@ -1,9 +1,7 @@
 package com.wang.qrcodecreateandscantool;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class QRCodeCreate extends ActionBarActivity {
@@ -12,27 +10,5 @@ public class QRCodeCreate extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_create);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_qrcode_create, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
